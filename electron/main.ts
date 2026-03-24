f498db0c390067f16b11b6a6f73caad345d933af
gunnerkring <gunnerkring@icloud.com>
2026-03-23 22:15:25 -0700

    Add macOS packaging support

--- electron/main.ts
+++ electron/main.ts
@@ -37,6 +37,12 @@ app.whenReady().then(() => {
   createWindow();
 });
 
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
